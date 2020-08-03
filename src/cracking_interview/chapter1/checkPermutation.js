9ff6109ca7b9977d14b121afa43eaa1ad546c054
ngtrhieu <ngtrhieu0011@gmail.com>
2020-08-03 14:51:08 +0800

    fix: update problem number

--- src/cracking_interview/chapter1/checkPermutation.js
+++ src/cracking_interview/chapter1/checkPermutation.js
@@ -3,7 +3,7 @@ import isAnagram from '../../problems/isAnagram';
 /**
  * Crack the Coding Interview
  * Chapter 1
- * Problem 2
+ * Problem 4
  * Given two strings, write a method to decide if one is a permutation of the
  * other.
  */
